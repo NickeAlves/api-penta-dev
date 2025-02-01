3a6177cbc16bbc096ef221bffdf83a1cef25fabd
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-02-01 12:11:16 -0300

    fixing bugs :bug:
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

--- src/main/java/com/api_penta_dev/models/User.java
+++ src/main/java/com/api_penta_dev/models/User.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "users")
+@Document(collection = "user")
 public class User {
     @Id
     private String id;
--- src/main/java/com/api_penta_dev/repositories/UserRepository.java
+++ src/main/java/com/api_penta_dev/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.api_penta_dev.repositories;
 
 import com.api_penta_dev.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
